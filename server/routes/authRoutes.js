8d54c9c3c6079abf08b27a5f00bbb0943e415100
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-15 13:16:03 +1000

    Added signin and signup validator

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const { authControllers } = require('../controllers/auth/authControllers')
+const Joi = require('joi')
+const validator = require('express-joi-validation').createValidator({})
 
-router.post('/signup', authControllers.postSignup)
+const signupSchema = Joi.object({
+	username: Joi.string().min(3).max(12).required(),
+	password: Joi.string().min(6).max(12).required(),
+	email: Joi.string().email().required(),
+})
 
-router.post('/signin', authControllers.postSignin)
+const signinSchema = Joi.object({
+	password: Joi.string().min(6).max(12).required(),
+	email: Joi.string().email().required(),
+})
+
+router.post('/signup', validator.body(signupSchema), authControllers.postSignup)
+router.post('/signin', validator.body(signinSchema), authControllers.postSignin)
 
 module.exports = router
