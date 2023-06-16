19bca1786936523257c61e31e2784d35e57292ab
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-16 12:45:26 +1000

    Finished set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from 'react-router-dom'
+import SigninPage from './authPages/SigninPage/SigninPage'
+import SignupPage from './authPages/SignupPage/SignupPage'
+import Dashboard from './Dashboard/Dashboard'
+import './App.css'
 
 function App() {
-	return <div className='App'>Hi there!</div>
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route exact path='/signin'>
+						<SigninPage />
+					</Route>
+					<Route exact path='/signup'>
+						<SignupPage />
+					</Route>
+					<Route exact path='/dashboard'>
+						<Dashboard />
+					</Route>
+					<Route path='/'>
+						<Navigate to='/dashboard' />
+					</Route>
+				</Routes>
+			</Router>
+		</>
+	)
 }
 
 export default App
