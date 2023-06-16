0e43996a0e2daf67a0107cfbf37cd7b99c1be4d1
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-16 15:04:18 +1000

    Create redirect info

--- client/src/pages/authPages/SigninPage/SigninPageFooter.js
+++ client/src/pages/authPages/SigninPage/SigninPageFooter.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import PrimaryButton from '../../../components/PrimaryButton'
+import RedirectInfo from '../../../components/RedirectInfo'
+import { useNavigate } from 'react-router-dom'
 
 const SigninPageFooter = ({ handleSignin, isFormValid }) => {
+	const navigate = useNavigate()
+
+	const handlePushToSignupPage = () => {
+		navigate('/signup')
+	}
+
 	return (
-		<div>
-			<PrimaryButton
-				label='Sign in'
-				onClick={handleSignin}
-				disabled={!isFormValid}
-				additionalStyles={{ marginTop: '30px' }}
+		<>
+			<div>
+				<PrimaryButton
+					label='Sign in'
+					onClick={handleSignin}
+					disabled={!isFormValid}
+					additionalStyles={{ marginTop: '30px' }}
+				/>
+			</div>
+			<RedirectInfo
+				text='Need an account? '
+				redirectText='Create an account'
+				additionalStyles={{ marginTop: '5px' }}
+				redirectHandler={handlePushToSignupPage}
 			/>
-		</div>
+		</>
 	)
 }
 
